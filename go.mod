6a1b068ce3ef9bb734c62c9e228bf926558379af
Ash Hellwig <ash@ashwigltd.com>
2020-06-25 23:25:48 -0600

    this is annoying me so ill take a break

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/ashwig/cannalysis_api
 
 go 1.14
 
-require github.com/joho/godotenv v1.3.0
+require (
+	github.com/abbot/go-http-auth v0.4.0
+	github.com/joho/godotenv v1.3.0
+	golang.org/x/net v0.0.0-20200625001655-4c5254603344 // indirect
+)
--- main.go
+++ main.go
@@ -1,55 +1,74 @@
 package main
 
 import (
+	"encoding/base64"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
 
+	auth "github.com/abbot/go-http-auth"
 	"github.com/joho/godotenv"
 )
 
-func makeAPIKey(vendorKey string, userKey string) string {
-	return vendorKey + ":" + userKey
-}
-
-func makeMETRCAuthURI(METRCURI string, vendorKey string, userKey string) string {
-	return METRCURI + makeAPIKey(vendorKey, userKey)
-}
-
-func main() {
+func getVendorKey() string {
 	// Import environment variables for configuration
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatalf("Error loading environment configuration: %v", err)
 	}
 
-	vendorKey := os.Getenv("METRC_VENDOR_KEY")
-	userKey := os.Getenv("METRC_USER_KEY")
-	metrcAuthURI := makeMETRCAuthURI(os.Getenv("METRC_URI"), vendorKey, userKey)
+	return os.Getenv("METRC_VENDOR_KEY")
+}
 
+func getUserKey() string {
+	// Import environment variables for configuration
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Error loading environment configuration: %v", err)
+	}
+
+	return os.Getenv("METRC_USER_KEY")
+}
+
+func getAPIKey() string {
+	// Import environment variables for configuration
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Error loading environment configuration: %v", err)
+	}
+
+	return os.Getenv("METRC_API_KEY")
+}
+
+func generateURI() string {
+	// Import environment variables for configuration
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Error loading environment configuration: %v", err)
+	}
+
+	metrcURIBase := os.Getenv("METRC_URI")
+
+	return string(getVendorKey() + ":" + getUserKey() + "@" + metrcURIBase)
+}
+
+func secret() string {
+	return string(base64.RawStdEncoding.EncodeToString([]byte(getAPIKey())))
+}
+
+func handle(w http.ResponseWriter, r *auth.AuthenticatedRequest) {
+	return
+}
+
+func main() {
 	client := &http.Client{}
-	if err != nil {
-		log.Fatalf("Error creating HTTP client: %v", err)
-	}
 
-	resp, err := client.Get(os.Getenv("METRC_URI"))
-	if err != nil {
-		log.Fatalf("Error reading HTTP response: %v", err)
-	}
-	defer resp.Body.Close()
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		log.Fatalf("Error reading HTTP response: %v", err)
-	}
-	log.Printf("Auth: %s", metrcAuthURI)
-	log.Printf("Response:\n-----\n%v", body)
-
-	req, err := http.NewRequest("GET", "/plants/v1/active/0", nil)
+	req, err := http.NewRequest(http.MethodGet, string(generateURI()+"/packages/v1/0"), nil)
 	if err != nil {
 		log.Fatalf("Error creating HTTP request: %v", err)
 	} else {
-		req.Header.Add("Authorization", makeAPIKey(vendorKey, userKey))
+		req.Header.Add("Authorization", string("Basic "+os.Getenv("METRC_ENCODED_API")))
 		req.Header.Add("Content-Type", "application/json")
 		resp, err := client.Do(req)
 		if err != nil {
@@ -60,7 +79,7 @@ func main() {
 		if err != nil {
 			log.Fatalf("Error reading HTTP response: %v", err)
 		}
-		log.Printf("Response:\n-----\n%v", body)
+		log.Printf("Response(%v):\n-----\n%v", string(resp.StatusCode), body)
 
 		// Output our response to the terminal (stdout)
 		log.Printf(`The response is below:
